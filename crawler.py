b5d907d637725a1a811af9bcba3abd635f1eaf4d
jjmmwon <jmw1790@gmail.com>
2024-07-12 18:08:33 +0900

    update crawler

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import requests
 from bs4 import BeautifulSoup
@@ -11,7 +12,7 @@ class Crawler:
         self.params = {"ref": "pagination", "page": 1} if params is None else params
         self.headers = (
             {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
                 "Accept-Language": "en-US,en;q=0.9",
                 "Accept-Encoding": "gzip, deflate, br",
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -41,10 +42,12 @@ class Crawler:
             "MyAIbeauties",
         ]
         self.titles = []
+        self.meta_df = None
 
     def _get_content(self, url=None, params=None):
         url = self.url if url is None else url
         response = requests.get(url, params=params, headers=self.headers)
+        time.sleep(1)
         if response.status_code != 200:
             print(response.content)
             print(response.status_code)
@@ -56,8 +59,12 @@ class Crawler:
     def set_negatives(
         self, negative_keywords: list = None, negative_sellers: list = None
     ):
-        self.negative_keywords += negative_keywords
-        self.negative_sellers += negative_sellers
+        self.negative_keywords += (
+            negative_keywords if negative_keywords is not None else []
+        )
+        self.negative_sellers += (
+            negative_sellers if negative_sellers is not None else []
+        )
 
     def _get_meta(self, item):
         title = item.find(class_="v2-listing-card__title").get_text(strip=True)
@@ -87,64 +94,18 @@ class Crawler:
             price = price.get_text(strip=True)
             detail_url = detail_url["href"]
 
-        content = self._get_content(detail_url)
-        if content is None:
-            return None
-
-        detail_soup = BeautifulSoup(content, "html.parser")
-
-        seller = detail_soup.find("a", class_="wt-text-link-no-underline wt-text-black")
-        description = detail_soup.find("div", {"data-id": "description-text"})
-        seller_img_list = detail_soup.findAll(
-            "li", class_="carousel-pagination-item-v2"
-        )
-        review_img_list = detail_soup.find(
-            "div", attrs={"data-appears-component-name": "customer_photos"}
-        )
-
-        if seller is None or description is None:
-            return None
-        else:
-            seller = seller.get_text(strip=True)
-            description = description.find("p").get_text(strip=True)
-
-        num_seller_img = len(seller_img_list) if seller_img_list else 0
-        num_review_img = len(review_img_list.find_all("img")) if review_img_list else 0
-
-        if any(neg in seller for neg in self.negative_sellers):
-            return None
-
         return {
             "title": title,
             "price": price,
-            "seller": seller,
-            "num_seller_img": num_seller_img,
-            "num_review_img": num_review_img,
             "rating": rating,
             "num_reviews": num_reviews,
-            "reviews": None,
-            "description": description,
             "detail_url": detail_url,
             "img_src": img_src,
         }
 
-    def crawl(
-        self, img_path: str = None, meta_path: str = None, num_items: int = 1000
-    ) -> pd.DataFrame:
-        """
-        Crawl and save images and metadata from Etsy.
-
-        Parameters
-        ----------
-        img_path: path to save images
-        meta_path: path to save metadata
-        num_items: number of items to crawl
-
-        Returns
-        -------
-        return: metadata dataframe
-        """
+    def crawl(self, content: str, meta_path=None):
         meta_path = "metadata" if meta_path is None else meta_path
+
         if not os.path.exists(meta_path):
             os.makedirs(meta_path)
 
@@ -152,69 +113,129 @@ class Crawler:
             "id",
             "title",
             "price",
-            "seller",
-            "num_seller_img",
-            "num_review_img",
+            # "seller",
             "rating",
             "num_reviews",
-            "reviews",
-            "description",
             "detail_url",
             "img_src",
+            # "num_seller_img",
+            # "num_review_img",
+            # "reviews",
+            # "description"
         ]
+
         if os.path.exists(f"{meta_path}/metadata.csv"):
-            meta_df = pd.read_csv(f"{meta_path}/metadata.csv", index_col=0)
-            img_src_list = list(meta_df["img_src"])
-            self.titles = meta_df["title"].tolist()
+            self.meta_df = pd.read_csv(f"{meta_path}/metadata.csv")
+            self.titles = self.meta_df["title"].tolist()
         else:
-            meta_df = pd.DataFrame(columns=columns)
-            img_src_list = []
+            self.meta_df = pd.DataFrame(columns=columns)
+            self.titles = []
 
-        for page in range(1, 201):
-            self.params["page"] = page
-            self.titles = meta_df["title"].tolist()
+        soup = BeautifulSoup(content, "html.parser")
+        items = soup.find_all("div", class_="v2-listing-card")
 
-            content = self._get_content(self.url, self.params)
-            if content is None:
+        for item in items:
+            meta = self._get_meta(item)
+
+            if meta is None:
                 continue
 
-            soup = BeautifulSoup(content, "html.parser")
-            items = soup.find_all("div", class_="v2-listing-card")
+            self.titles.append(meta["title"])
+            meta["id"] = str(len(self.meta_df) + 1)
 
-            for item in items:
-                meta = self._get_meta(item)
+            new_row = pd.DataFrame([meta], columns=columns)
+            self.meta_df = pd.concat([self.meta_df, new_row])
 
-                if meta is None:
-                    continue
-                self.titles.append(meta["title"])
+        self.meta_df.to_csv(f"{meta_path}/metadata.csv", index=False)
 
-                meta["id"] = len(meta_df) + 1
-                img_src_list.append(meta["img_src"])
+        return self.meta_df
 
-                new_row = pd.DataFrame([meta], columns=columns)
-                meta_df = pd.concat([meta_df, new_row])
-
-                if len(meta_df) % 10 == 0:
-                    print(f"Progress: {len(meta_df)}/{num_items}")
-
-                if len(meta_df) >= num_items:
-                    break
-
-            meta_df.to_csv(f"{meta_path}/metadata.csv", index=False)
-            if len(meta_df) >= num_items:
-                break
-
-        meta_df.to_csv(f"{meta_path}/metadata.csv", index=False)
-        self.save_images(img_src_list, img_path)
-
-        return meta_df
-
-    def save_images(self, img_src_list: list, img_path: str = None):
-        img_path = "images" if img_path is None else img_path
+    def save_images(self, img_path: str = None):
+        img_path = "images"
         if not os.path.exists(img_path):
             os.makedirs(img_path)
+
+        if self.meta_df is None:
+            raise ValueError("Metadata not found.")
+
+        img_src_list = list(self.meta_df["img_src"])
 
         for i, img_src in enumerate(img_src_list):
             img = requests.get(img_src)
             with open(f"{img_path}/{i+1}.jpg", "wb") as f:
                 f.write(img.content)
+
+    def load_meta(self, meta_path=None):
+        meta_path = "metadata/metadata.csv" if meta_path is None else meta_path
+
+        if not os.path.exists(meta_path):
+            raise ValueError(f"Metadata not found in {meta_path}")
+
+        self.meta_df = pd.read_csv(f"{meta_path}")
+
+        return self
+
+    def get_details(self):
+        detail_df = pd.DataFrame(
+            columns=[
+                "id",
+                "seller",
+                "num_seller_img",
+                "num_review_img",
+                "description",
+            ]
+        )
+        for _, data in self.meta_df.iterrows():
+            detail_url = data["detail_url"]
+
+            content = self._get_content(detail_url)
+
+            if content is None:
+                return None
+
+            detail_soup = BeautifulSoup(content, "html.parser")
+
+            seller = detail_soup.find(
+                "a", class_="wt-text-link-no-underline wt-text-black"
+            )
+            description = detail_soup.find("div", {"data-id": "description-text"})
+            seller_img_list = detail_soup.findAll(
+                "li", class_="carousel-pagination-item-v2"
+            )
+            review_img_list = detail_soup.find(
+                "div", attrs={"data-appears-component-name": "customer_photos"}
+            )
+
+            if seller is None or description is None:
+                seller = "None"
+                description = "None"
+                num_review_img = 0
+                num_seller_img = 0
+            else:
+                seller = seller.get_text(strip=True)
+                description = description.find("p").get_text(strip=True)
+
+                num_seller_img = len(seller_img_list) if seller_img_list else 0
+                num_review_img = (
+                    len(review_img_list.find_all("img")) if review_img_list else 0
+                )
+
+            if any(neg in seller for neg in self.negative_sellers):
+                return None
+
+            new_row = pd.DataFrame(
+                [
+                    {
+                        "id": data["id"],
+                        "seller": seller,
+                        "num_seller_img": num_seller_img,
+                        "num_review_img": num_review_img,
+                        "description": description,
+                    }
+                ]
+            )
+            detail_df = pd.concat([detail_df, new_row])
+
+        self.meta_df = pd.merge(self.meta_df, detail_df, on="id")
+
+        return self.meta_df
